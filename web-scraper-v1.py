ec8d8bbcd4556ff0cff3d8435bbcc3d4362e7cfa
baylnb <baylenbrus@gmail.com>
2024-05-15 03:06:33 -0700

    simple loop logic

--- web-scraper-v1.py
+++ web-scraper-v1.py
@@ -1,7 +1,7 @@
 import requests
 import os
 #from requests.adapters import HTTPAdapter
-#from tenacity import retry
+from tenacity import retry
 
 def clear_all_cookies(session):
     session.cookies.clear()
@@ -26,9 +26,11 @@ def simple_request_handler_check():
     #for cookie_name, cookie_value in cookies.items():
      #   session.cookies.set(cookie_name, cookie_value)
       #  print(f"Session.Cookies var is: {session.cookies}, Cookie_Name var is: {cookie_name}, Cookie_Value var is: {cookie_value}")
+    
+    counter = 0
+    max_retries = 40
 
-    while True:
-        counter = 0
+    while counter < max_retries:
         for domain in domains:
             for tld in tlds:
                 if domain == "nhentai":
@@ -38,19 +40,16 @@ def simple_request_handler_check():
                 url = f"https://www.{domain}.{tld}"
                 try:
                     response = session.get(url)
-                    counter = counter + 1
-                    print(f"Count: {counter}: New Line Status code for {url}: {response.status_code}")
+                    print(f"Count: {counter}: \n Status code for {url}: {response.status_code}")
                     if response.status_code == 403: 
+                        counter+=1
                         #print(f"Requests to {domain} have the following headers:{response.request.headers}")
                         print(f"The cookies before: {session.cookies}")
                         clear_all_cookies(session)
                         print(f"The cookies after: {session.cookies}")
                 except requests.exceptions.RequestException as e:
                     print(f"Error for {url}: {e}")
-                else:
-                    if counter >= 40:
-                        break
-
+                    counter+=1 
             #print(response)
 
 simple_request_handler_check()
